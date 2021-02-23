4d135815e349c3dfe9df8cd576ccc07c6eb1e5e6
Sat Daya Khalsa <satdayasingh@gmail.com>
2021-02-23 11:14:00 -0800

    Update 4. EOW Inventory Including Demand.sql
    Updated query and added documentation

--- 4. EOW Inventory Including Demand.sql
+++ 4. EOW Inventory Including Demand.sql
@@ -1,5 +1,12 @@
-DROP TABLE IF EXISTS [available_dev];
+/*
+This query examines the availability at the end of each week. It does so by:
+ a. establishing the inventory at the end of prior week
+ b. adding the incoming PO volume
+ c. subtracting the anticipated demand
+ A Case statement eliminates all negative quantities.
+*/
 
+DROP TABLE IF EXISTS [available_dev];
 
 CREATE TABLE [available_dev]( 
    [item_id]     VARCHAR(54) PRIMARY KEY
@@ -26,253 +33,243 @@ WITH
    ,[5_wk_out_recv]
    ,[6_wk_out_recv]
 
-	)
+    )
   AS (
      SELECT 
-	    DISTINCT [item_id]
-	   ,SUM([1_wk_out_recv])
-	   ,SUM([2_wk_out_recv])
-	   ,SUM([3_wk_out_recv])
-	   ,SUM([4_wk_out_recv])
-	   ,SUM([5_wk_out_recv])
-	   ,SUM([6_wk_out_recv])
-	 FROM [po_la_deux_cleaned]
-	 GROUP BY [item_id]
-	 )
+        DISTINCT [item_id]
+       ,SUM([1_wk_out_recv])
+       ,SUM([2_wk_out_recv])
+       ,SUM([3_wk_out_recv])
+       ,SUM([4_wk_out_recv])
+       ,SUM([5_wk_out_recv])
+       ,SUM([6_wk_out_recv])
+     FROM [po_la_deux_cleaned]
+     GROUP BY [item_id]
+     )
 ,
   [cte_demand_agg]
   (
      [item_id]
-	,[1_wk_out]
-	,[2_wk_out]
-	,[3_wk_out]
-	,[4_wk_out]
-	,[5_wk_out]
-	,[6_wk_out]
-	)
+    ,[1_wk_out]
+    ,[2_wk_out]
+    ,[3_wk_out]
+    ,[4_wk_out]
+    ,[5_wk_out]
+    ,[6_wk_out]
+    )
   AS (
      SELECT 
-	    DISTINCT [item_id]
+        DISTINCT [item_id]
        ,SUM([1_wk_out])
-	   ,SUM([2_wk_out])
-	   ,SUM([3_wk_out])
-	   ,SUM([4_wk_out])
-	   ,SUM([5_wk_out])
-	   ,SUM([6_wk_out])
+       ,SUM([2_wk_out])
+       ,SUM([3_wk_out])
+       ,SUM([4_wk_out])
+       ,SUM([5_wk_out])
+       ,SUM([6_wk_out])
      FROM  [projctd_dmnd_weekly_buckets]
-	 GROUP BY [item_id]
-	 )
+     GROUP BY [item_id]
+     )
 ,
   [cte_wk1_out]
     ( 
      [item_id]
-	,[eow_oh]
-	)
+    ,[eow_oh]
+    )
   AS (
     SELECT
-	   DISTINCT [current_inventory].[short_partnumber]
-	  ,CASE WHEN ( 
-	             SUM( ISNULL( [current_inventory].[qty_oh], 0 ) )
-	             + 
-	             SUM( ISNULL( [cte_po_la_deux].[1_wk_out_recv], 0 ) )
-				 - 
-				 SUM( ISNULL( [cte_demand_agg].[1_wk_out], 0 ) ) 
-				 )
-				 < 0
-	        THEN 0
-			ELSE (
-			     SUM( ISNULL( [current_inventory].[qty_oh], 0 ) )
-	             + 
-	             SUM( ISNULL( [cte_po_la_deux].[1_wk_out_recv], 0 ) )
-				 - 
-				 SUM( ISNULL( [cte_demand_agg].[1_wk_out], 0 ) )
-				 )
+       DISTINCT [current_inventory].[short_partnumber]
+      ,CASE WHEN ( 
+                 SUM( ISNULL( [current_inventory].[qty_oh], 0 ) )
+                 + 
+                 SUM( ISNULL( [cte_po_la_deux].[1_wk_out_recv], 0 ) )
+                 - 
+                 SUM( ISNULL( [cte_demand_agg].[1_wk_out], 0 ) ) 
+                 )
+                 < 0
+            THEN 0
+            ELSE (
+                 SUM( ISNULL( [current_inventory].[qty_oh], 0 ) )
+                 + 
+                 SUM( ISNULL( [cte_po_la_deux].[1_wk_out_recv], 0 ) )
+                 - 
+                 SUM( ISNULL( [cte_demand_agg].[1_wk_out], 0 ) )
+                 )
             END
-	FROM [current_inventory]
-	LEFT JOIN [cte_po_la_deux] 
-	  ON [current_inventory].[short_partnumber] = [cte_po_la_deux].[item_id]
-	LEFT JOIN [cte_demand_agg]
-	  ON [current_inventory].[short_partnumber] = [cte_demand_agg].[item_id]
-	GROUP BY [current_inventory].[short_partnumber]
+    FROM [current_inventory]
+    LEFT JOIN [cte_po_la_deux] 
+      ON [current_inventory].[short_partnumber] = [cte_po_la_deux].[item_id]
+    LEFT JOIN [cte_demand_agg]
+      ON [current_inventory].[short_partnumber] = [cte_demand_agg].[item_id]
+    GROUP BY [current_inventory].[short_partnumber]
 
-	  )
+      )
 ,
 
   [cte_wk2_out]
    ( 
      [item_id]
-	,[eow_oh]
-	)
+    ,[eow_oh]
+    )
   AS (
     SELECT
-	   DISTINCT [current_inventory].[short_partnumber]
-	  ,CASE WHEN (
-	             SUM( ISNULL( [cte_wk1_out].[eow_oh], 0 ) )
-	             +
-	             SUM( ISNULL( [cte_po_la_deux].[2_wk_out_recv], 0 ) )
-				 - 
-				 SUM( ISNULL([cte_demand_agg].[2_wk_out], 0 ) )
-				 )
-				 < 0
-	        THEN 0
-			ELSE (
-			     SUM( ISNULL([cte_wk1_out].[eow_oh], 0 ) )
-	             +
-	             SUM( ISNULL([cte_po_la_deux].[2_wk_out_recv], 0 ) )
-				 - 
-				 SUM( ISNULL([cte_demand_agg].[2_wk_out], 0 ) )
-				 )
-			END
-	FROM [current_inventory]
-	LEFT JOIN [cte_po_la_deux] 
-	  ON [current_inventory].[short_partnumber] = [cte_po_la_deux].[item_id]
-	LEFT JOIN [cte_demand_agg]
-	  ON [current_inventory].[short_partnumber] = [cte_demand_agg].[item_id]
-	LEFT JOIN [cte_wk1_out]
-	  ON [current_inventory].[short_partnumber] = [cte_wk1_out].[item_id]
-    GROUP BY [current_inventory].[short_partnumber]
-	  )
+       DISTINCT [cte_wk1_out].[item_id]
+      ,CASE WHEN (
+                 SUM( ISNULL( [cte_wk1_out].[eow_oh], 0 ) )
+                 +
+                 SUM( ISNULL( [cte_po_la_deux].[2_wk_out_recv], 0 ) )
+                 - 
+                 SUM( ISNULL([cte_demand_agg].[2_wk_out], 0 ) )
+                 )
+                 < 0
+            THEN 0
+            ELSE (
+                 SUM( ISNULL([cte_wk1_out].[eow_oh], 0 ) )
+                 +
+                 SUM( ISNULL([cte_po_la_deux].[2_wk_out_recv], 0 ) )
+                 - 
+                 SUM( ISNULL([cte_demand_agg].[2_wk_out], 0 ) )
+                 )
+            END
+    FROM [cte_wk1_out]
+    LEFT JOIN [cte_po_la_deux] 
+      ON [cte_wk1_out].[item_id] = [cte_po_la_deux].[item_id]
+    LEFT JOIN [cte_demand_agg]
+      ON [cte_wk1_out].[item_id] = [cte_demand_agg].[item_id]
+    GROUP BY [cte_wk1_out].[item_id]
+      )
 ,
 
   [cte_wk3_out]
    ( 
      [item_id]
-	,[eow_oh]
-	)
+    ,[eow_oh]
+    )
   AS (
     SELECT
-	   DISTINCT [current_inventory].[short_partnumber]
-	  ,CASE WHEN (
-	             SUM( ISNULL([cte_wk2_out].[eow_oh], 0 ) )
-	             +
-	             SUM( ISNULL([cte_po_la_deux].[3_wk_out_recv], 0 ) )
-				 -
-				 SUM (ISNULL([cte_demand_agg].[3_wk_out], 0 ) )
-				 )
-				 < 0
-	        THEN 0
-			ELSE (
-			     SUM( ISNULL([cte_wk2_out].[eow_oh], 0 ) )
-	             +
-	             SUM( ISNULL([cte_po_la_deux].[3_wk_out_recv], 0 ) )
-				 -
-				 SUM (ISNULL([cte_demand_agg].[3_wk_out], 0 ) )
-				 )
-			END
-	FROM [current_inventory]
-	LEFT JOIN [cte_po_la_deux] 
-	  ON [current_inventory].[short_partnumber] = [cte_po_la_deux].[item_id]
-	LEFT JOIN [cte_demand_agg]
-	  ON [current_inventory].[short_partnumber] = [cte_demand_agg].[item_id]
-	LEFT JOIN [cte_wk2_out]
-	  ON [current_inventory].[short_partnumber] = [cte_wk2_out].[item_id]
-	GROUP BY [current_inventory].[short_partnumber]
-	  )
+       DISTINCT [cte_wk2_out].[item_id]
+      ,CASE WHEN (
+                 SUM( ISNULL([cte_wk2_out].[eow_oh], 0 ) )
+                 +
+                 SUM( ISNULL([cte_po_la_deux].[3_wk_out_recv], 0 ) )
+                 -
+                 SUM (ISNULL([cte_demand_agg].[3_wk_out], 0 ) )
+                 )
+                 < 0
+            THEN 0
+            ELSE (
+                 SUM( ISNULL([cte_wk2_out].[eow_oh], 0 ) )
+                 +
+                 SUM( ISNULL([cte_po_la_deux].[3_wk_out_recv], 0 ) )
+                 -
+                 SUM (ISNULL([cte_demand_agg].[3_wk_out], 0 ) )
+                 )
+            END
+    FROM [cte_wk2_out]
+    LEFT JOIN [cte_po_la_deux] 
+      ON [cte_wk2_out].[item_id] = [cte_po_la_deux].[item_id]
+    LEFT JOIN [cte_demand_agg]
+      ON [cte_wk2_out].[item_id]  = [cte_demand_agg].[item_id]
+    GROUP BY [cte_wk2_out].[item_id]
+      )
 ,
   [cte_wk4_out]
    ( 
      [item_id]
-	,[eow_oh]
-	)
+    ,[eow_oh]
+    )
   AS (
     SELECT
-	   DISTINCT [current_inventory].[short_partnumber]
-	  ,CASE WHEN (
-	             SUM( ISNULL([cte_wk3_out].[eow_oh], 0 ) )
-	             +
-				 SUM( ISNULL([cte_po_la_deux].[4_wk_out_recv], 0 ) )
-				 -
-				 SUM (ISNULL([cte_demand_agg].[4_wk_out], 0 ) )
-				 )
-				 < 0
-	        THEN 0
-			ELSE (
-			     SUM( ISNULL([cte_wk3_out].[eow_oh], 0 ) )
-	             +
-				 SUM ( ISNULL([cte_po_la_deux].[4_wk_out_recv], 0 ) )
-				 -
-				 SUM ( ISNULL([cte_demand_agg].[4_wk_out], 0 ) )
-				 )
-			END
-	FROM [current_inventory]
-	LEFT JOIN [cte_po_la_deux] 
-	  ON [current_inventory].[short_partnumber] = [cte_po_la_deux].[item_id]
-	LEFT JOIN [cte_demand_agg]
-	  ON [current_inventory].[short_partnumber] = [cte_demand_agg].[item_id]
-	LEFT JOIN [cte_wk3_out]
-	  ON [current_inventory].[short_partnumber] = [cte_wk3_out].[item_id]
-	GROUP BY [current_inventory].[short_partnumber]
-	  )
+       DISTINCT [cte_wk3_out].[item_id]
+      ,CASE WHEN (
+                 SUM( ISNULL([cte_wk3_out].[eow_oh], 0 ) )
+                 +
+                 SUM( ISNULL([cte_po_la_deux].[4_wk_out_recv], 0 ) )
+                 -
+                 SUM (ISNULL([cte_demand_agg].[4_wk_out], 0 ) )
+                 )
+                 < 0
+            THEN 0
+            ELSE (
+                 SUM( ISNULL([cte_wk3_out].[eow_oh], 0 ) )
+                 +
+                 SUM ( ISNULL([cte_po_la_deux].[4_wk_out_recv], 0 ) )
+                 -
+                 SUM ( ISNULL([cte_demand_agg].[4_wk_out], 0 ) )
+                 )
+            END
+    FROM [cte_wk3_out]
+    LEFT JOIN [cte_po_la_deux] 
+      ON [cte_wk3_out].[item_id] = [cte_po_la_deux].[item_id]
+    LEFT JOIN [cte_demand_agg]
+      ON [cte_wk3_out].[item_id] = [cte_demand_agg].[item_id]
+    GROUP BY [cte_wk3_out].[item_id]
+      )
 ,
   [cte_wk5_out]
    ( 
      [item_id]
-	,[eow_oh]
-	)
+    ,[eow_oh]
+    )
   AS (
     SELECT
-	   DISTINCT [current_inventory].[short_partnumber]
-	  ,CASE WHEN (
-	             SUM( ISNULL([cte_wk4_out].[eow_oh], 0 ) )
-	             +
-				 SUM( ISNULL([cte_po_la_deux].[5_wk_out_recv], 0 ) )
-				 -
-				 SUM (ISNULL([cte_demand_agg].[5_wk_out], 0 ) )
-				 )
-				 < 0
-	        THEN 0
-			ELSE (
-	             SUM( ISNULL([cte_wk4_out].[eow_oh], 0 ) )
-	             +
-				 SUM( ISNULL([cte_po_la_deux].[5_wk_out_recv], 0 ) )
-				 -
-				 SUM (ISNULL([cte_demand_agg].[5_wk_out], 0 ) )
-				 )
-			END
-	FROM [current_inventory]
-	LEFT JOIN [cte_po_la_deux] 
-	  ON [current_inventory].[short_partnumber] = [cte_po_la_deux].[item_id]
-	LEFT JOIN [cte_demand_agg]
-	  ON [current_inventory].[short_partnumber] = [cte_demand_agg].[item_id]
-	LEFT JOIN [cte_wk4_out]
-	  ON [current_inventory].[short_partnumber] = [cte_wk4_out].[item_id]
-	GROUP BY [current_inventory].[short_partnumber]
-	  )
+       DISTINCT [cte_wk4_out].[item_id]
+      ,CASE WHEN (
+                 SUM( ISNULL([cte_wk4_out].[eow_oh], 0 ) )
+                 +
+                 SUM( ISNULL([cte_po_la_deux].[5_wk_out_recv], 0 ) )
+                 -
+                 SUM (ISNULL([cte_demand_agg].[5_wk_out], 0 ) )
+                 )
+                 < 0
+            THEN 0
+            ELSE (
+                 SUM( ISNULL([cte_wk4_out].[eow_oh], 0 ) )
+                 +
+                 SUM( ISNULL([cte_po_la_deux].[5_wk_out_recv], 0 ) )
+                 -
+                 SUM (ISNULL([cte_demand_agg].[5_wk_out], 0 ) )
+                 )
+            END
+    FROM [cte_wk4_out]
+    LEFT JOIN [cte_po_la_deux]
+      ON [cte_wk4_out].[item_id] = [cte_po_la_deux].[item_id]
+    LEFT JOIN [cte_demand_agg]
+      ON [cte_wk4_out].[item_id] = [cte_demand_agg].[item_id]
+    GROUP BY [cte_wk4_out].[item_id]
+      )
 ,
   [cte_wk6_out]
    ( 
      [item_id]
-	,[eow_oh]
-	)
+    ,[eow_oh]
+    )
   AS (
     SELECT
-	   DISTINCT [current_inventory].[short_partnumber]
-	  ,CASE WHEN (
-	             SUM( ISNULL([cte_wk5_out].[eow_oh], 0 ) )
-	             +
-				 SUM( ISNULL([cte_po_la_deux].[6_wk_out_recv], 0 ) )
-				 -
-				 SUM (ISNULL([cte_demand_agg].[6_wk_out], 0 ) )
-				 )
-				 < 0
-	        THEN 0
-			ELSE (
-	             SUM( ISNULL([cte_wk5_out].[eow_oh], 0 ) )
-	             +
-				 SUM( ISNULL([cte_po_la_deux].[6_wk_out_recv], 0 ) )
-				 -
-				 SUM (ISNULL([cte_demand_agg].[6_wk_out], 0 ) )
-				 )
-			END
-	FROM [current_inventory]
-	LEFT JOIN [cte_po_la_deux] 
-	  ON [current_inventory].[short_partnumber] = [cte_po_la_deux].[item_id]
-	LEFT JOIN [cte_demand_agg]
-	  ON [current_inventory].[short_partnumber] = [cte_demand_agg].[item_id]
-	LEFT JOIN [cte_wk5_out]
-	  ON [current_inventory].[short_partnumber] = [cte_wk5_out].[item_id]
-	GROUP BY [current_inventory].[short_partnumber]
-	  )
+       DISTINCT [cte_wk5_out].[item_id]
+      ,CASE WHEN (
+                 SUM( ISNULL([cte_wk5_out].[eow_oh], 0 ) )
+                 +
+                 SUM( ISNULL([cte_po_la_deux].[6_wk_out_recv], 0 ) )
+                 -
+                 SUM (ISNULL([cte_demand_agg].[6_wk_out], 0 ) )
+                 )
+                 < 0
+            THEN 0
+            ELSE (
+                 SUM( ISNULL([cte_wk5_out].[eow_oh], 0 ) )
+                 +
+                 SUM( ISNULL([cte_po_la_deux].[6_wk_out_recv], 0 ) )
+                 -
+                 SUM (ISNULL([cte_demand_agg].[6_wk_out], 0 ) )
+                 )
+            END
+    FROM [cte_wk5_out]
+    LEFT JOIN [cte_po_la_deux] 
+      ON [cte_wk5_out].[item_id] = [cte_po_la_deux].[item_id]
+    LEFT JOIN [cte_demand_agg]
+      ON [cte_wk5_out].[item_id] = [cte_demand_agg].[item_id]
+    GROUP BY [cte_wk5_out].[item_id]
+      )
 
 INSERT INTO [available_dev]
    (
